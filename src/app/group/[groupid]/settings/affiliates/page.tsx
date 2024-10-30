376f65e0534547877bee932045fb8a2592dfcba4
Arbab Kamal <146167220+arbab-kamal@users.noreply.github.com>
2024-10-30 19:53:23 +0530

    Update page.tsx

--- src/app/group/[groupid]/settings/affiliates/page.tsx
+++ src/app/group/[groupid]/settings/affiliates/page.tsx
@@ -20,7 +20,7 @@ const Affiliate = async ({ params }: Props) => {
            https://grouple-kohl.vercel.app/affiliates/
             {affiliate.affiliate?.id}
             <CopyButton
-              content={`http://localhost:3000/affiliates/${affiliate.affiliate?.id}`}
+              content={`https://grouple-kohl.vercel.app/affiliates/${affiliate.affiliate?.id}`}
             />
           </div>
           <CardDescription className="text-themeTextGray">
